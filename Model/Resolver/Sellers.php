2339d0d525c2e50adb15b470b473b90ea153ef6d
MSG Chinh <45973976+msgchinh@users.noreply.github.com>
2021-05-03 14:35:43 +0700

    Update graphql (#12)
    * update GraphQl get list seller
    
    * update readme
    
    * update graphql get seller list
    
    * update graphql get seller list
    
    * add seller rating to list sellers
    
    * create seller
    
    * create graphql create seller
    
    * fix bug graphql create seller
    
    * update create seller
    
    * update graphql add seller info to product
    
    * fix bug get list product
    
    * update copy right, add graphql create rating seller
    
    * update copy right
    
    Co-authored-by: chinhnvc98 <vanchinhpc9.com>
    Co-authored-by: Chinhnvc98 <45973976+Chinhnvc98@users.noreply.github.com>

--- Model/Resolver/Sellers.php
+++ Model/Resolver/Sellers.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Landofcoder
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/terms
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lof_MarketPlaceGraphQl
+ * @copyright  Copyright (c) 2021 Landofcoder (https://www.landofcoder.com/)
+ * @license    https://landofcoder.com/terms
+ */
 
 namespace Lof\MarketplaceGraphQl\Model\Resolver;
 
@@ -23,6 +42,14 @@ class Sellers extends AbstractSellerQuery implements ResolverInterface {
      */
     private $sellers;
 
+    /**
+     * Sellers constructor.
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param SellersFrontendRepositoryInterface $seller
+     * @param SellerProductsRepositoryInterface $productSeller
+     * @param ProductRepositoryInterface $productRepository
+     * @param Products\Query\SellerQueryInterface $sellers
+     */
     public function __construct(
         SearchCriteriaBuilder $searchCriteriaBuilder,
         SellersFrontendRepositoryInterface $seller,
